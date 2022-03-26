6872725a16df5dc5407ce2c839713be1c92c0695
Denis Kolodin <deniskolodin@gmail.com>
2022-03-26 10:00:18 +0200

    Add OptionExt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 use thiserror::Error;
 
+pub trait OptionExt<T> {
+    fn required(self) -> Result<T, Required>;
+}
+
+impl<T> OptionExt<T> for Option<T> {
+    fn required(self) -> Result<T, Required> {
+        self.ok_or(Required)
+    }
+}
+
 /// If option or parameter is required.
 #[derive(Error, Debug)]
 #[error("value is required")]
